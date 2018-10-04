057d17d70c9c2679773abd69b561531c167812ab
Antoni Dabrowski <amd146@duke.edu>
2018-10-03 22:28:12 -0400

    final minus analysis

--- src/WordGram.java
+++ src/WordGram.java
@@ -1,25 +1,27 @@
+
+import java.util.Arrays;
 /**
  * WordGram objects represent a k-gram of strings/words.
- * 
- * @author YOUR-NAME
- *
+ * @author Antoni Dabrowski
  */
-
 public class WordGram {
 	
 	private String[] myWords;   
 	private String myToString;  // cached string
 	private int myHash;         // cached hash value
-
+	
 	/**
-	 * Create WordGram (add comments)
-	 * @param source
-	 * @param start
-	 * @param size
+	 * Create object WordGram containing size strings copied from the array source
+	 * @param source is an array of strings 
+	 * @param start is the index of source from which strings start being copied
+	 * @param size is the number of strings that will end up in WordGram
 	 */
 	public WordGram(String[] source, int start, int size) {
+		
 		myWords = new String[size];
-		// TODO: initialize myWords and ...
+		myWords = Arrays.copyOfRange(source, start, start+size);
+		myHash = 0;
+		myToString = null;
 	}
 
 	/**
@@ -35,12 +37,11 @@ public class WordGram {
 	}
 
 	/**
-	 * Complete this comment
-	 * @return
+	 * This method will return the order or size of the WordGram
+	 * @return length of myWords, the size of the WordGram
 	 */
 	public int length(){
-		// TODO: change this
-		return 0;
+		return myWords.length;
 	}
 
 
@@ -49,32 +50,41 @@ public class WordGram {
 		if (! (o instanceof WordGram) || o == null){
 			return false;
 		}
-
-	    // TODO: complete this method
-		return true;
+		WordGram wg = (WordGram) o;
+		if (Arrays.equals(wg.myWords, this.myWords)) {
+			return true;
+		} else {
+			return false;
+		}
 	}
 
 	@Override
 	public int hashCode(){
-		// TODO: complete this method
+		myHash = this.toString().hashCode();
 		return myHash;
 	}
 	
 
 	/**
-	 * Create and complete this comment
+	 * create a new WordGram object wg with same number of entries as original WordGram(this) whose first k-1 entries are the
+	 *   same as the last k-1 entries of the original WordGram(this), and whose last entry is the parameter last.
 	 * @param last is last String of returned WordGram
-	 * @return
+	 * @return the new WordGram wg
 	 */
 	public WordGram shiftAdd(String last) {
-		WordGram wg = new WordGram(myWords,0,myWords.length);
-		// TODO: Complete this method
+		String[] wgNew = new String[this.length()];
+		for (int i = 0; i < this.length() - 1; i++) {
+			wgNew[i] = myWords[i+1];
+		}
+		wgNew[this.length() - 1] = last;
+		WordGram wg = new WordGram(wgNew,0,this.length());
+		//System.out.print(wg);  used this to test new wordgram and make sure it is being created correctly
 		return wg;
 	}
 
 	@Override
 	public String toString(){
-		// TODO: Complete this method	
+		myToString = String.join(" ", myWords);
 		return myToString;
 	}
 }
